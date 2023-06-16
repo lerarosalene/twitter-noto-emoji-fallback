2b1ca76272f0bb64c265132221d6a901448d9181
Larissa Rosalene <lerarosalene@outlook.com>
2023-06-17 02:04:17 +0300

    fix switch flickering on popup opening

--- src/popup.ts
+++ src/popup.ts
@@ -4,6 +4,7 @@ import { Message, MessageType, message } from "./messages";
 class PopupScript {
   private _port: chrome.runtime.Port | null = null;
   private _check: HTMLInputElement | null = null;
+  private _main: HTMLDivElement | null = null;
 
   private handleChange(evt: Event) {
     if (!evt.target || !(evt.target instanceof HTMLInputElement)) {
@@ -21,6 +22,7 @@ class PopupScript {
     if (msg.type === MessageType.SETTINGS_RESPONSE && this._check) {
       this._check.checked = msg.replaceAll;
     }
+    this._main?.classList.remove('hidden');
   }
 
   start() {
@@ -28,6 +30,7 @@ class PopupScript {
     this._port.postMessage(message({ type: MessageType.SETTINGS_REQUEST }));
     this._check = document.querySelector("#replace-all");
     this._check?.addEventListener("change", this.handleChange.bind(this));
+    this._main = document.querySelector('#main');
     this._port.onMessage.addListener(this.handleMessage.bind(this));
   }
 }
